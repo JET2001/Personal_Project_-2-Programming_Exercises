e1d85ff23efd251308a90692d2202d9b6ef1d2b5
Jonathan Teo <91585955+JET2001@users.noreply.github.com>
2021-10-01 13:37:36 +0800

    Add files via upload

---
+++ code_files/q1/1_avgweight.c
@@ -0,0 +1,28 @@
+//
+//  4_avgweight.c
+//  
+//
+//  Created by Jon on 29/7/21.
+//
+
+#include <stdio.h>
+
+// Prototype
+long double avgweight(const char filename[]);
+// Driver code
+int main(void)
+{
+    char filename[50];
+    scanf("%s", filename);
+    printf("%.10Lf\n", avgweight(filename));
+    return 0;
+}
+
+// ----- Implementation below -----
+long double avgweight(const char filename[])
+{
+    // YOUR CODE HERE ---------
+    
+    
+    return 0.0;
+}
